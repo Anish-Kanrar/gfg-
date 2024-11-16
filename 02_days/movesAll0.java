b13171a17d4b84c94ba40f43770be51479b49eb3
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-11-16 23:52:59 +0530

    .

---
+++ 02_days/movesAll0.java
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+         int j=-1;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0){
+                j=i;
+                break;
+            }
+        }
+        
+        if(j == -1) 
+            return a;
+        
+        for(int i=j+1;i<n;i++){
+            if(arr[i] != 0){
+                swap(arr[i],arr[j]);
+                j++;
+            }
+        }
+        return a;
+            }
+};
