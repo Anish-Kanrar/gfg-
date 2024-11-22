f3d0276e30deeb43e6098b27ae38bddf914bd73e
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-11-23 00:03:47 +0530

    arr

---
+++ 04_days/rotatedArray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+          int first = 0, last = arr.length - 1;
+        while (first < last) {
+            int temp;
+            temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+}
