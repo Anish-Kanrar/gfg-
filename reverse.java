d6446562dfe543da919a1c54ad7173d109432a74
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2025-03-15 00:02:41 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,18 @@
+package LInkedList;
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev=null;
+        Node curr=head;
+        Node next;
+        while(curr != null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+
+}
